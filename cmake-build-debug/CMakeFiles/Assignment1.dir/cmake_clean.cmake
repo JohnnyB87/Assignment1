9ec36cad7b1929580738c7d990a3183056140e3f
JohnnyB87 <johnbrady1987@gmail.com>
2018-03-08 12:36:12 +0000

    reset git

---
+++ cmake-build-debug/CMakeFiles/Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment1.dir/main.c.obj"
+  "CMakeFiles/Assignment1.dir/pal.c.obj"
+  "CMakeFiles/Assignment1.dir/usefull.c.obj"
+  "Assignment1.pdb"
+  "Assignment1.exe"
+  "Assignment1.exe.manifest"
+  "libAssignment1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
